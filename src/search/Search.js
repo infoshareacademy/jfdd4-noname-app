6b26bb2c7f14afe66e8a538173355e62f8609a43
Pamela Szymaniec <pamela.szymaniec@gmail.com>
2016-10-12 22:08:23 +0200

    filter names of busstops

---
+++ src/search/Search.js
@@ -0,0 +1,24 @@
+import React, {Component} from 'react'
+
+class Search extends Component {
+    filterUpdate() {
+        const val = this.myValue.value;
+
+        this.props.filterUpdate(val);
+    }
+
+    render() {
+        return (
+            <form>
+                <input
+                    type="text"
+                    ref={ (value) => {this.myValue = value} }
+                    placeholder="Wpisz szukany przystanek ..."
+                    onChange={this.filterUpdate.bind(this)}
+                />
+            </form>
+        )
+    }
+}
+
+export default Search

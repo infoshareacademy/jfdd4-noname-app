c34fd468a429423e787d4dd8085999526dff5afc
Piotr Szefler <piotr.szefler@gmail.com>
2016-10-12 16:29:44 +0200

    add basic bus & stops data

--- src/data/data.js
+++ src/data/data.js
@@ -1,4 +1,22 @@
 
 var data = {
-    stops: [{id: 1, name: Przeróbka}]
-}
+    stops: [
+        {id: 1, name: "Przeróbka"},
+        {id: 2, name: "Głęboka"},
+        {id: 3, name: "Brama Żuławska"},
+        {id: 4, name: "Akademia Muzyczna"},
+        {id: 5, name: "Szafarnia"},
+        {id: 6, name: "Dolne Miasto"},
+        {id: 7, name: "Śluza"},
+        {id: 8, name: "Kamienna Grobla"},
+        {id: 9, name: "Muzeum Narodowe"},
+        {id: 10, name: "Brama Wyżynna"}
+    ],
+    buses: [
+        {id: 111, stops: [1, 3, 4, 7]},
+        {id: 112, stops: [2, 3, 5, 6, 8]},
+        {id: 178, stops: [3, 5, 6, 7, 8]},
+        {id: 199, stops: [1, 2, 3, 4, 5]},
+        {id: 200, stops: [4, 5, 6, 7, 8]}
+    ]
+};

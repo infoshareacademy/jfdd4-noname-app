ed872b00964bd7baf16193490019cde24098d282
kgodlewski2 <godlewskikar@gmail.com>
2016-10-12 22:40:43 +0200

    component outlook...

--- src/bus-details/BusDetails.js
+++ src/bus-details/BusDetails.js
@@ -22,16 +22,17 @@ export default class BusDetails extends React.Component {
         return (
             <div>
                  <p>Szczegóły linia - 139</p>
-                 <p>Kier.: Oliwa | PKP Przymorze SKM - Kołobrzeska - Dąbrowszczaków - Jagiellońska
+                 <p>Kier.: Oliwa | PKP Przymorze SKM - Kołobrzeska - <strong>Dąbrowszczaków</strong> - Jagiellońska
                      - Chłopska - Bora-Komorowskiego - Droszyńskiego - Oliwa PKP </p>
                 <p>Kier.: Przymorze SKM | Oliwa PKP - Bora-Komorowskiego - Chłopska - Jagiellońska
                     - Dąbrowszczaków - Obrońców Wybrzeża - Chłopska - Kołobrzeska - Przymorze SKM</p>
 
-                <p>Kierunek: Przymorze SKM</p>
+                <p>Kierunek: <strong>Oliwa</strong></p>
                 <p>Przystanki</p>
                 <ul>
                     <li>Przystanek1</li>
                     <li>Przystanek2</li>
+                    <li><strong>Dąbrowszczaków - click for details</strong></li>
                     <li>Przystanek...</li>
                 </ul>
 

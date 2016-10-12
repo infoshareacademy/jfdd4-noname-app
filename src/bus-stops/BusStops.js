9a79d8e76734bc7b2cd1d06f57a3f437c0d3d20a
Pamela Szymaniec <pamela.szymaniec@gmail.com>
2016-10-12 16:57:11 +0200

    first layout

--- src/bus-stops/BusStops.js
+++ src/bus-stops/BusStops.js
@@ -1,10 +1,29 @@
 import React from 'react'
 import data from '../data/data.js';
 
-class BusStops extends  React.Component {
+export default class BusStops extends React.Component {
+
+    constructor() {
+        super();
+
+        this.state = {
+            stops: []
+        }
+    }
+
+    componentWillMount() {
+        this.setState({
+            stops: data.stops
+        })
+    }
+
     render() {
+
         return (
-            console.log(data)
+            <div>{this.state.stops.map(function (stop) {
+                return <li key={stop.id}>{stop.name}</li>
+            })}</div>
+
         );
     }
 }

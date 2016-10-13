6b6f8a49b6269ca0d9c500e621071b5dec0112e3
Pamela Szymaniec <pamela.szymaniec@gmail.com>
2016-10-13 10:35:55 +0200

    move app to its own directory

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './app/App';
 import './index.css';
 import BusStops from './bus-stops/BusStops'
 import BusLines from './bus-lines/BusLines'

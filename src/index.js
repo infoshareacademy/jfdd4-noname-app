74388bbbbfe75eb65ee88db88b47d67530d8e357
Karol Godlewski <godlewskikar@gmail.com>
2016-10-14 17:06:47 +0200

    working filter

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ ReactDOM.render(
         <Route path="/" component={App}>
             <Route path="/bus-stops" component={BusStops}/>
             <Route path="/bus-lines" component={BusLines}/>
-            <Route path="/bus-details" component={BusDetails}/>
+            <Route path="/bus-details/:busId" component={BusDetails}/>
         </Route>
     </Router>,
   document.getElementById('root')

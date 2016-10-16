c6428339f8f0310ae7cdbddc5ab01216b6a37a92
Pamela Szymaniec <pamela.szymaniec@gmail.com>
2016-10-16 19:54:48 +0200

    change map component from default to class

--- src/map/Map.js
+++ src/map/Map.js
@@ -3,15 +3,18 @@ import React from 'react'
 import GoogleMap from 'google-map-react'
 import Place from './place/Place'
 
-export default (props) =>
-    <div style={{width: '300px', height: '300px'}}>
-        {props.x} {props.y}
-        <GoogleMap
-            bootstrapURLKeys={{
-                key: 'AIzaSyCkDbleAYeCPGyTEDJ8Jk94gwXDxombvRE'
-            }}
-            center={[54.357267, 18.682472]}
-            zoom={15}>
-            <Place lat={props.x} lng={props.y} text={'A'} />
-        </GoogleMap>
-    </div>
+export default  class Map extends React.Component {
+    render() {
+        return <div style={{width: '300px', height: '300px'}}>
+            {this.props.x} {this.props.y}
+            <GoogleMap
+                bootstrapURLKeys={{
+                    key: 'AIzaSyCkDbleAYeCPGyTEDJ8Jk94gwXDxombvRE'
+                }}
+                center={[this.props.x, this.props.y]}
+                zoom={15}>
+                <Place lat={this.props.x} lng={this.props.y} text={'A'}/>
+            </GoogleMap>
+        </div>
+    }
+}

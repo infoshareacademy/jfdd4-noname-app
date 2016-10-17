6f2b017c42dd48b34271a5ab52444abaa34f7d9f
Karol Godlewski <godlewskikar@gmail.com>
2016-10-17 15:50:19 +0200

    init

--- src/bus-details/BusDetails.js
+++ src/bus-details/BusDetails.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router';
 import data from '../data/data.js';
 
 
@@ -32,11 +33,19 @@ export default class BusDetails extends React.Component {
             <div>
                 <h2>{this.props.params.busId}</h2>
 
+                <ul>
                 {data.stops.filter(function(stop) {
                     return currentBus.stops.indexOf(stop.id) != -1
                 }).map(function(stop) {
-                    return (<li key={stop.id}>{stop.name}</li>)
+                    return (
+                        <li key={stop.id}>
+                            <Link to={`/bus-stops/${stop.id}`}>
+                                {stop.name}
+                            </Link>
+                        </li>
+                    )
                 })}
+                </ul>
 
             </div>
         )

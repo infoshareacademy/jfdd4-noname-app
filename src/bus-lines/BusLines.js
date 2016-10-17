956482ba4f71396c7d248097a1a8958fabb739c2
Karol Godlewski <godlewskikar@gmail.com>
2016-10-17 16:14:05 +0200

    component shows list off all bus lines

--- src/bus-lines/BusLines.js
+++ src/bus-lines/BusLines.js
@@ -1,4 +1,42 @@
-import React from 'react'
+import React from 'react';
+import data from '../data/data.js';
 
 
-export default (props) => <h1>Lista autobusów</h1>
+export default class BusLines extends React.Component{
+
+    constructor () {
+        super();
+
+        this.state = {
+            busesArray: []
+        }
+
+    }
+
+    componentWillMount () {
+        this.setState ({
+            busesArray: data.buses
+        })
+    }
+
+
+
+    render () {
+
+        var mapBusArray = this.state.busesArray;
+        console.log(mapBusArray);
+
+        return (
+            <div>
+                <h1>Lista linii autobusowych:</h1>
+                <ul>
+                    {mapBusArray.map(function (busListItem, index) {
+                        return <li key={index}>
+                            {busListItem.lineNumber}
+                        </li>
+                    })}
+                </ul>
+            </div>
+        )
+    }
+}

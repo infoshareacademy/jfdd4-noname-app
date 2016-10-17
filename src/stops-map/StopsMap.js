6c3bc4fa497387c2960a0ea443fd93f449648ee9
Pamela Szymaniec <pamela.szymaniec@gmail.com>
2016-10-17 16:26:46 +0200

    adding Stops map component

--- src/stops-map/StopsMap.js
+++ src/stops-map/StopsMap.js
@@ -1,17 +1,20 @@
 
 import React from 'react'
 import GoogleMap from 'google-map-react'
+import Stop from './stop-place/Stop'
+
+
 
 export default  class StopsMap extends React.Component {
     render() {
         return <div style={{width: '1100px', height: '500px'}}>
-            {this.props.x} {this.props.y}
             <GoogleMap
                 bootstrapURLKeys={{
                     key: 'AIzaSyCkDbleAYeCPGyTEDJ8Jk94gwXDxombvRE'
                 }}
                 center={[54.357267, 18.682472]}
                 zoom={10}>
+                <Stop lat={54.357267} lng={18.682472} text={'A'}/>
             </GoogleMap>
         </div>
     }

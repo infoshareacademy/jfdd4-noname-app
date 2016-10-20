bd43812b5f5873de7262147bc496fcd01c2e58d4
Pamela Szymaniec <pamela.szymaniec@gmail.com>
2016-10-20 16:16:07 +0200

    adding fav icons and animation

--- src/app/menu/Menu.js
+++ src/app/menu/Menu.js
@@ -1,34 +1,41 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router'
-import Item from './item/Item'
 import logo from './logo.png';
 import './Menu.css';
+import {IndexLinkContainer, LinkContainer} from 'react-router-bootstrap'
 import {Navbar, Nav, NavItem} from 'react-bootstrap'
 
 
 class Menu extends Component {
     render() {
         return (
-        <Navbar default>
-            <Navbar.Header>
-                <Navbar.Brand>
-                    <Link to={`/`}><img src={logo} className="Menu-logo" alt="logo"/></Link>
-                </Navbar.Brand>
-                <Navbar.Toggle />
-            </Navbar.Header>
-            <Navbar.Collapse>
-                <Nav>
-                    <Item path={`/`}>Mapa przystanków</Item>
-                    <Item path={`/bus-stops`}>Lista przystanków</Item>
-                     <Item path={`/bus-lines`}>Lista autobusów</Item>
-                </Nav>
-                <Nav pullRight>
-                    <NavItem eventKey={1} href="#">Zaloguj się</NavItem>
-                </Nav>
-            </Navbar.Collapse>
-        </Navbar>
-
-
+            <Navbar default>
+                <Navbar.Header>
+                    <Navbar.Brand>
+                        <Link to={`/`}><img src={logo} className="Menu-logo" alt="logo"/></Link>
+                    </Navbar.Brand>
+                    <Navbar.Toggle />
+                </Navbar.Header>
+                <Navbar.Collapse>
+                    <Nav>
+                        <IndexLinkContainer to={`/`}>
+                            <NavItem eventKey={0} href="#">Mapa przystanków</NavItem>
+                        </IndexLinkContainer>
+                        <LinkContainer to={`/bus-stops`}>
+                            <NavItem eventKey={1} href="#">Lista przystanków</NavItem>
+                        </LinkContainer>
+                        <LinkContainer to={`/bus-lines`}>
+                            <NavItem eventKey={2} href="#">Lista autobusów
+                            </NavItem>
+                        </LinkContainer>
+                    </Nav>
+                    <Nav pullRight>
+                        <LinkContainer to={'*'}>
+                            <NavItem eventKey={3} href="#">Zaloguj się</NavItem>
+                        </LinkContainer>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
         )
     }
 }

75375c2bbc117c76abe2b9315565fb60a91c7eb0
kgodlewski2 <godlewskikar@gmail.com>
2016-10-20 19:50:11 +0200

    line details assigns other lines passing through assigned bus stops

--- src/line-stops/LineStops.js
+++ src/line-stops/LineStops.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router';
-import {ListGroup, ListGroupItem} from 'react-bootstrap'
+import {ListGroup, ListGroupItem, Label} from 'react-bootstrap'
 import data from '../data/data.js'
 
 export default class LineStops extends React.Component {
 
     render() {
-        var zmapowaneStopy =  data.buses.map(function(xyz) {
-            return xyz;
-        });
 
         var currentStop;
 
@@ -24,11 +21,30 @@ export default class LineStops extends React.Component {
                     return (
                         <ListGroupItem key={stop.id}>
                             <Link to={`/bus-stops/${stop.id}`}>
-                                {stop.name}
-                                {currentStop = zmapowaneStopy.stops.filter(function(dupa){
-                               console.log(dupa)})
-                                }
+                                {stop.name + " "}
                             </Link>
+                                <div>
+                                    <content>Dostępne przesiadki:</content>
+                                {data.buses.filter(function(fff) {
+                                    console.log(fff, "ooooooooooooooo");
+                                    return currentStop = fff.stops.indexOf(stop.id) !== -1;
+                                }).map(function (dupa) {
+                                    console.log(dupa, "***********************");
+                                    console.log(dupa.lineNumber, "xxxxxxxxxxxxxxxxx");
+                                    return (
+                                        <content>
+                                            <content>{" "}</content>
+                                            <Label>
+                                                <Link to={`/bus-details/${dupa.lineNumber}`}>
+                                                    {dupa.lineNumber}
+                                                </Link>
+                                            </Label>
+                                        </content>
+                                    )
+                                })
+
+                                }
+                                </div>
                         </ListGroupItem>
                     )
                 })}

d8d5e69b128c398d29ee0fc60a5648169ce7bfbe
Karol Godlewski <godlewskikar@gmail.com>
2016-10-20 16:58:36 +0200

    init3

--- src/line-stops/LineStops.js
+++ src/line-stops/LineStops.js
@@ -1,24 +1,33 @@
 import React from 'react';
 import { Link } from 'react-router';
 import {ListGroup, ListGroupItem} from 'react-bootstrap'
+import data from '../data/data.js'
 
 export default class LineStops extends React.Component {
 
     render() {
+        var zmapowaneStopy =  data.buses.map(function(xyz) {
+            return xyz;
+        });
 
-        var przesiadka;
+        var currentStop;
 
         return (
 
             <ListGroup>
                 {this.props.stops.map(function (stop) {
+                    console.log(stop, "------");
+                    console.log(data.buses.map(function(xyz)
+                    {
+                      console.log(xyz, "++++++++++++")
+                    }));
                     return (
                         <ListGroupItem key={stop.id}>
                             <Link to={`/bus-stops/${stop.id}`}>
                                 {stop.name}
-                                {przesiadka = data.stops.find(function (czyMozliwa) {
-                                    czyMozliwa.id ===
-                                }) }
+                                {currentStop = zmapowaneStopy.stops.filter(function(dupa){
+                               console.log(dupa)})
+                                }
                             </Link>
                         </ListGroupItem>
                     )

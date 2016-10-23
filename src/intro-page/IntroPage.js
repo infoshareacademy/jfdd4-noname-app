8ef321b6d27f46a4822cab3421ce309eec8af1f7
pamela <pamela.szymaniec@gmail.com>
2016-10-23 20:43:46 +0200

    add icons

--- src/intro-page/IntroPage.js
+++ src/intro-page/IntroPage.js
@@ -1,15 +1,37 @@
 import React from 'react'
 import './IntroPage.css'
-
+import {Grid, Row, Col} from 'react-bootstrap'
+import functionbusicon from './functionbusicon.svg'
+import functionmapicon from './functionmapicon.svg'
+import functionticketicon from './functionticketicon.svg'
+import {Link} from 'react-router'
 
 export default class IntroPage extends React.Component {
 
     render() {
         return (
 
-            <div>
-               Hello
-            </div>
+            <Grid>
+                <Row className="Intro-icon">
+                    <Col sm={4} className="Intro-col">
+                        <ul className="Intro-icon">
+                            <li><Link to={`/`}><img src={functionbusicon} className="Intro-img" alt="logo"/></Link></li>
+                        </ul>
+                    </Col>
+                    <Col sm={4} className="Intro-col">
+                        <ul className="Intro-icon">
+                            <li><Link to={`/`}><img src={functionmapicon} className="Intro-img" alt="logo"/></Link></li>
+                        </ul>
+                    </Col>
+                    <Col sm={4}  className="Intro-col">
+                        <ul className="Intro-icon">
+                            <li><Link to={`/`}><img src={functionticketicon} className="Intro-img" alt="logo"/></Link></li>
+                        </ul>
+                    </Col>
+                </Row>
+
+
+            </Grid>
 
         )
     }

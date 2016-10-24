1a03eefa40818df012f8edad8587796f7e15fd35
Pamela Szymaniec <pamela.szymaniec@gmail.com>
2016-10-24 14:34:46 +0200

    sort bus stops - remove sorting in action creator

--- src/intro-page/IntroPage.js
+++ src/intro-page/IntroPage.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import './IntroPage.css'
-import {Grid, Row, Col} from 'react-bootstrap'
+import {Col} from 'react-bootstrap'
 import functionbusicon from './functionbusicon.svg'
 import functionmapicon from './functionmapicon.svg'
 import busstop from './busstop.svg'
@@ -11,27 +11,28 @@ export default class IntroPage extends React.Component {
     render() {
         return (
             <div>
-                <Col sm={12} className="Intro-col">
+                <Col sm={12} className="Intro-col ">
+
                     <h2> Zaplanuj optymalnie swoją podróż!
                     </h2>
                 </Col>
                 <Col sm={4} className="Intro-col">
-                    <div className="Intro-icon"><Link to={`/`}><img src={functionbusicon} className="Intro-img"
-                                                                    alt="bus"/></Link>
+                    <Link to={`/bus-lines`}><div className="Intro-icon"><img src={functionbusicon} className="Intro-img"
+                                                                    alt="bus"/>
                         <div>Linie autobusowe</div>
-                    </div>
+                    </div></Link>
                 </Col>
                 <Col sm={4} className="Intro-col">
-                    <div className="Intro-icon"><Link to={`/`}><img src={functionmapicon} className="Intro-img"
-                                                                    alt="map"/></Link>
+                    <Link to={`/map`}><div className="Intro-icon"><img src={functionmapicon} className="Intro-img"
+                                                                    alt="map"/>
                         <div>Mapa przystanków</div>
-                    </div>
+                    </div></Link>
                 </Col>
                 <Col sm={4} className="Intro-col">
-                    <div className="Intro-icon"><Link to={`/`}><img src={busstop} className="Intro-img"
-                                                                    alt="busstop"/></Link>
+            <Link to={`/bus-stops`}><div className="Intro-icon"><img src={busstop} className="Intro-img"
+                                                                    alt="busstop"/>
                         <div>Lista przystanków</div>
-                    </div>
+                    </div></Link>
 
                 </Col>
 

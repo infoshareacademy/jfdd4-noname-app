ec210c5c89119efed61cbddf7c4cf54c29b210ba
kgodlewski2 <godlewskikar@gmail.com>
2016-11-06 22:42:20 +0100

    time tables fixed again

--- src/bus-stop/BusStop.js
+++ src/bus-stop/BusStop.js
@@ -4,16 +4,14 @@ import {Link} from 'react-router';
 import {Label} from 'react-bootstrap'
 import Map from '../map/Map'
 import {markStopAsFavorite} from '../bus-stops/actionCreators'
-import {Glyphicon, Button, Col, Row, Panel} from 'react-bootstrap'
+import {Glyphicon, Button, Col, Panel} from 'react-bootstrap'
 import './Bus.css'
 import {addFavoriteStop, deleteFavoriteStop} from '../favorites/actionCreators'
-import IncomingBuses from '../incoming-buses/IncomingBuses'
 import busstopicon from './busstopicon.gif';
 
 const mapStateToProps = (state) => ({
     buses: state.busesData.buses,
     stops: state.stopsData.stops,
-    hourValue: state.sliderData.hourValue,
     favoriteStops: state.favorites.favoriteStops
 });
 
@@ -22,26 +20,18 @@ const mapDispatchToProps = (dispatch) => ({
     deleteFavoriteStop: (stopId) => dispatch(deleteFavoriteStop(stopId))
 });
 
-const mapHourStringToMinutes = (hourString) =>
-    parseInt(hourString.slice(0,2))*60 + parseInt(hourString.slice(3,5));
-
-
 class BusStop extends React.Component {
 
     render() {
         var {
             buses,
             stops,
-            favouriteStop,
-            hourValue,
             addFavoriteStop,
             deleteFavoriteStop,
             favoriteStops
         } = this.props;
 
-
         var stopId = parseInt(this.props.params.busStopId);
-        var busId = buses;
         var currentStop = stops.filter(stop => stop.id === stopId);
         var currentCoordinates = currentStop.map((stop) => {
             return [stop.cox, stop.coy]
@@ -51,13 +41,15 @@ class BusStop extends React.Component {
         return (
             <div>
                 <Col sm={12} className="Intro-col ">
+
+
                     {currentStop.map(function (stop) {
                         console.log('TEST 2',favoriteStops, favoriteStops.indexOf(stopId), stopId)
                         return <p>Przystanek: {stop.name} {""}
                             <Button onClick={() => {
-                            favoriteStops.map(a=>a.id).indexOf(stopId) === -1 ?
-                            addFavoriteStop(stopId) :
-                            deleteFavoriteStop(stopId)
+                                favoriteStops.map(a=>a.id).indexOf(stopId) === -1 ?
+                                    addFavoriteStop(stopId) :
+                                    deleteFavoriteStop(stopId)
                             }} bsSize="xsmall">
                                 <Glyphicon glyph="star"/>
                                 {favoriteStops.map(a=>a.id).indexOf(stopId) === -1 ? "Dodaj do ulubionych" : "Usuń z ulubionych"}
@@ -77,43 +69,40 @@ class BusStop extends React.Component {
                 </Col>
                 <Col sm={6} className="BusStops-col">
                     {buses.filter(function (bus) {
-                })}
-                    <Col sm={6} className="BusStops-col">
-                        {buses.filter(function (bus) {
-                            return bus.stops.indexOf(stopId) !== -1
-                        }).map(function (bus) {
-                                let busStopIndex = bus.stops.indexOf(stopId);
-                                return (
-                                    <Col sm={6} className="BusStop-col">
+                        return bus.stops.indexOf(stopId) !== -1
+                    }).map(function (bus) {
+                            let busStopIndex = bus.stops.indexOf(stopId);
+                            return (
+                                <Col sm={6} className="BusStop-col">
 
-                                        <div>
-                                            <Panel header={<div><Link to={`/bus-details/${bus.lineNumber}`}>
-                                                <img src={busstopicon} alt="busstopicon"/>
-                                                <Label style={{'margin': '2px'}}>{bus.lineNumber}</Label>
-                                            </Link>
-                                                <p>Przystanek: {currentStop.map(function (stop) {
-                                                    return <span> {stop.name} </span>
-                                                })}</p>
-                                            </div>
-                                            }>
-                                                <p>{bus.routes.map(function (route) {
-                                                    return <b>{route[busStopIndex] + ' '}</b>
-                                                })}</p>
-                                            </Panel>
+                                    <div>
+                                        <Panel header={<div><Link to={`/bus-details/${bus.lineNumber}`}>
+                                            <img src={busstopicon} alt="busstopicon"/>
+                                            <Label style={{'margin': '2px'}}>{bus.lineNumber}</Label>
+                                        </Link>
+                                            <p>Przystanek: {currentStop.map(function (stop) {
+                                                return <span> {stop.name} </span>
+                                            })}</p>
                                         </div>
-                                    </Col>
-                                )
-                            }
-                        )}
+                                        }>
+                                            <p>{bus.routes.map(function (route) {
+                                                return <b>{route[busStopIndex] + ' '}</b>
+                                            })}</p>
+                                        </Panel>
+                                    </div>
+                                </Col>
+                            )
+                        }
+                    )}
 
-                    </Col>
                 </Col>
                 <Col sm={6} className="Map-col">
                     <div style={{width: '100%', height: '450px'}}>
                         <Map center={currentCoordinates[0]} points={currentStop}/>
                     </div>
-                </Col>
 
+
+                </Col>
             </div>
         );
     }
